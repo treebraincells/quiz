2abfdb3cff4d750d1a99cf05775dafeecc93c1b7
xXTgamerXx <trinethr_n@pm.me>
2022-12-02 18:25:31 +0530

    added files

---
+++ questions.js
@@ -0,0 +1,167 @@
+let questions = [
+    {
+    numb: 1,
+    question: "How many gold medals has India won in olympics?",
+    answer: "10",
+    options: [
+      "15",
+      "12",
+      "10",
+      "17"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How far did Neeraj Chopra Javelin travel at the Tokyo Olympics 2021 Finals",
+    answer: "87.58m",
+    options: [
+      "86.77m",
+      "91.04m",
+      "89.27m",
+      "87.58m"
+    ]
+  },
+  {
+    numb: 3,
+    question: "When was the opening ceremony of the Tokyo Olympics 2021 held?",
+    answer: "23, July, 2021",
+    options: [
+      "23, July, 2021",
+      "20, July, 2021",
+      "27, July, 2021",
+      "25, July, 2021"
+    ]
+  },
+  {
+    numb: 4,
+    question: "When was the closing ceremony of the Tokyo Olympics 2021 held?",
+    answer: "8, August, 2021",
+    options: [
+      "6, August, 2021",
+      "8, August, 2021",
+      "9, August, 2021",
+      "7, August, 2021"
+    ]
+  },
+  {
+    numb: 5,
+    question: "How many sports were held in the Tokyo Olympics 2021?",
+    answer: "33",
+    options: [
+      "37",
+      "35",
+      "45",
+      "33"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which country won the Tokyo Olympics 2021",
+    answer: "USA",
+    options: [
+      "Australlia",
+      "USA",
+      "China",
+      "Japan"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which country came at last in the Tokyo Olympics 2021",
+    answer: "Syria",
+    options: [
+      "Austria",
+      "Syria",
+      "Moldova",
+      "Iceland"
+    ]
+  },
+  {
+    numb: 8,
+    question: "How many gold medals did USA win overall in the olympics?",
+    answer: "1061",
+    options: [
+      "1041",
+      "10057",
+      "1061",
+      "1011"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which medal did PV Sindhu get in Tokyo olympics?",
+    answer: "Bronze",
+    options: [
+      "Gold",
+      "Sliver",
+      "Bronze",
+      "None"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which weight category did Mirabai Chanu participate in?",
+    answer: "49kg",
+    options: [
+      "59kg",
+      "49kg",
+      "69kg",
+      "Mirabai Chanu did not participate"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Who did Neeraj Chopra play against in Tokyo Olympics 2021?",
+    answer: "Johannes Vetter",
+    options: [
+      "Johannes Vetter",
+      "Keshorn Walcott",
+      "Anderson Peters",
+      "Julius Yego"
+    ]
+  },
+  {
+    numb: 12,
+    question: "How much weight did Mirabai Chanu lift (in total) in the Tokyo Olympics 2021?",
+    answer: "196",
+    options: [
+      "204",
+      "197",
+      "196",
+      "173"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Who was the first ever Indian fencer to qualify for The Tokyo Olympics 2021?",
+    answer: "Bhavani Devi",
+    options: [
+      "Shilpa Garg",
+      "Pooja Mishra",
+      "Bhavani Devi",
+      "India did not qualify"
+    ]
+  },
+  {
+    numb: 14,
+    question: "What is the Tokyo Olympics 2020 Mascot?",
+    answer: "Miraitowa",
+    options: [
+      "PyeongChang",
+      "Miraitowa",
+      "Nagano",
+      "Phevos and Athena"
+    ]
+  },
+  {
+    numb: 15,
+    question: "What do the five rings on the Olympic symbol represent?",
+    answer: "Five continents",
+    options: [
+      "Five continents",
+      "Trust",
+      "Honor",
+      "Freedom"
+    ]
+  },
+];
